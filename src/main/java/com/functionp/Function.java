ea2561de3d1b688ca7b91c84bbdf9f8fcdeb9486
Andy Lewis <alewis@microsoft.com>
2019-04-19 20:19:43 -0400

    enable anonymous users

--- src/main/java/com/functionp/Function.java
+++ src/main/java/com/functionp/Function.java
@@ -17,7 +17,7 @@ public class Function {
      */
     @FunctionName("HttpTrigger-Java")
     public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
